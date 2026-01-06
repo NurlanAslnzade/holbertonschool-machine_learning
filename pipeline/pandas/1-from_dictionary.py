7a402571241e07abb3d023aa59507f463ac5286e
Your Name <you@example.com>
2026-01-06 21:24:37 +0000

    s

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 """salam"""
 import pandas as pd
-df = pd.DataFrame({"First": [0.0, 0.5, 1.0, 1.5], "Second": ["one", "two", "three", "four"]}, index = ["A", "B", "C", "D"])
+
+
+sutun = {'First': [0.0, 0.5, 1.0, 1.5],
+       'Second': ['one', 'two', 'three', 'four']}
+df = pd.DataFrame(sutun, index=list("ABCD"))
