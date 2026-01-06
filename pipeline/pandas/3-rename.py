b64c4eab1daf4d4d19bd604e53f281d384b8cf0d
Your Name <you@example.com>
2026-01-06 22:12:45 +0000

    s

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""ssds sadasdasdas"""
+import pandas as pd
+
+def rename(df):
+    """"asdasdas dasdasdasdas """"
+    df = df.rename(columns = {"Timestamp" : "Datetime"})
+    df['Datetime'] = pd.to_datetime(df['Datetime'], unit="s")
+    return df.loc[:, ["Datetime", "Close"]]
