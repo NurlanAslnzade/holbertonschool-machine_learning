60b1eb221ff8e8e5e0ce2f2ad1780b5c0374ede9
Your Name <you@example.com>
2026-01-07 11:30:57 +0000

    s

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python3
-"""tasd  dksadmas kms"""
-import pandas as pd
-
-
 def array(df):
     """asdasdsadasda sdaasd a"""
     df = df.tail(10)
-    df = df.shape(1)
     df = df[["High". "Close"]].to_numpy()
     return df
