6dee5a33a92bf27e8781b50facdb3a172297a417
Your Name <you@example.com>
2026-01-07 12:06:18 +0000

    s

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -4,4 +4,4 @@
 
 def slice(df):
     """asdasdsadasda sdaasd a"""
-    return df.loc[60, ["High", "Low", "Close", "Volume_BTC"]]
+    return df.loc[["High", "Low", "Close", "Volume_BTC"]].iloc[::60]
