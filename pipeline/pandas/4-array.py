41020e76d1323486cc4017801dd574c1a4d839cc
Your Name <you@example.com>
2026-01-07 11:29:09 +0000

    s

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""tasd  dksadmas kms"""
+import pandas as pd
+
+
+def array(df):
+    """asdasdsadasda sdaasd a"""
+    df = df.tail(10)
+    df = df.shape(1)
+    df = df[["High". "Close"]].to_numpy()
+    return df
