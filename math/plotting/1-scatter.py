4ea2ecbe191e170094d77d415abaaa72aa586f50
Your Name <you@example.com>
2026-01-11 01:26:57 +0300

    s

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""plot yaratmaq"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def scatter():
+    """scatter plot yaratmaq"""
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(x, y, marker='o', color='magenta', linestyle="None")
+    plt.title("Men's Height vs Weight")
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
