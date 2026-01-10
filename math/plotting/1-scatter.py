3eddbcff9a97eed15f78a91a74016eee2f093216
Your Name <you@example.com>
2026-01-11 01:30:08 +0300

    s

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -3,12 +3,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def scatter():
     """scatter plot yaratmaq"""
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
-    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    x, y = np.random.multivariate_normal(mean, cov, 2000)
     y += 180
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(x, y, marker='o', color='magenta', linestyle="None")
