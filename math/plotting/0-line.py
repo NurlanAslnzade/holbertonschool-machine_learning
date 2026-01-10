f1c0e36e4556cdad0a37639af4eb4ef350bd9344
Your Name <you@example.com>
2026-01-11 01:11:08 +0300

    s

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
+"""plot yaratmaq"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line():
 
+def line():
+"""plot yaratmaq"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(y, color="red")
     plt.margins(x=0)
-
-    figure.set_xlims(0,10)
-    plt.show()
+    plt.xlim(0, 10))
