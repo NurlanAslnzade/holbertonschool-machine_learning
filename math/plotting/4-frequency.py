c6b11fee39e4b0e9380e38ac232a9c170643a484
Your Name <you@example.com>
2026-01-11 12:05:09 +0000

    s

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
+"""histogram of student scores"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def frequency():
 
+def frequency():
+    """
+    The x-axis should be labeled Grades
+    The y-axis should be labeled Number of Students
+    The x-axis should have bins every 10 units
+    The title should be Project A
+    The bars should be outlined in black
+    """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
     bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
-    plt.title("Projec A")
-    plt.hist(student_grades, bins=bins, edgecolor="black")
+    plt.title("Project A")
+    plt.hist(student_grades, bins=bins, edgecolor='black')
     plt.xticks(bins)
     plt.xlim(0, 100)
-    plt.ylim(0,30)
+    plt.ylim(0, 30)
