dd4ead51098f145acba7dc8230fab3a4ca373740
Your Name <you@example.com>
2026-01-24 10:41:47 +0000

    s

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""sadasdas dasda"""
+
+
+def add_arrays(arr1, arr2):
+    """arraylar cemi"""
+    if len(arr1) != len(arr2):
+        return None
+    new_list = []
+    for i in range(len(arr1)):
+        new_list.append(arr1[i] + arr2[i])
+    return new_list
