4033f4a0967be2185fe540ca1b50e2a44f0e5dda
Your Name <you@example.com>
2026-01-24 10:53:22 +0000

    s

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""sadasdas dasda"""
+
+
+def add_matrices2D(mat1, mat2):
+    """arraylar cemi"""
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    new_list = []
+    for i in range(len(mat1)):
+        new = []
+        for k in range(len(mat1[0])):
+            new.append(mat1[k][i] + mat2[k][i])
+        new_list.append(new)
+    return new_list
