014e9f1d6e217a63f9545d8bbefafb5432ff4301
Your Name <you@example.com>
2026-02-07 07:19:04 +0000

    s

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -39,7 +39,7 @@ def minor(matrix):
         for j in range(n):
             # Submatrix excluding row i, column j
             submatrix = [r[:j] + r[j + 1:]
-            for r in (matrix[:i] + matrix[i + 1:])]
-                row.append(determinant(submatrix))
+                    for r in (matrix[:i] + matrix[i + 1:])]
+                    row.append(determinant(submatrix))
         minor_mat.append(row)
     return minor_mat
