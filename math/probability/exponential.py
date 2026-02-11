388fa5ce3f7ac552f8b537b6fa93817e39b0387e
Your Name <you@example.com>
2026-02-11 16:48:01 +0000

    s

---
+++ math/probability/exponential.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Script to calculate a Exponential distribution
+"""
+
+
+class Exponential:
+    """
+    class to call the methods of exponential distribution
+    """
+
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        init
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = lambtha
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = (1 / (sum(data) / len(data)))
+
+    def pdf(self, x):
+        """
+        calculating pdf of exponential distribution
+        """
+        if x < 0:
+            return 0
+        pdf = self.lambtha * Exponential.e**((-self.lambtha) * x)
+        return pdf
+
+    def cdf(self, x):
+        """
+        calculating cdf of exponential distribution
+        """
+        if x < 0:
+            return 0
+        cdf = 1 - Exponential.e**((-self.lambtha) * x)
+        return cdf
