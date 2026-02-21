505ecc466e78114f4fe0e96e3fb1753df515d9d9
Your Name <you@example.com>
2026-02-21 07:53:41 +0000

    s

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python3
+"""Module that contains the function create_confusion_matrix"""
 import numpy as np
-"""ASDasdasd asdas da"""
 
 
 def create_confusion_matrix(labels, logits):
     """
-    Creates a confusion matrix from one-hot encoded labels and logits.
-    
-    Args:
-        labels: numpy.ndarray of shape (m, classes) with one-hot true labels
-        logits: numpy.ndarray of shape (m, classes) with one-hot predicted labels
-    
-    Returns:
-        numpy.ndarray of shape (classes, classes) confusion matrix
+    Returning confusion matrix
     """
-    y_true = np.argmax(labels, axis=1)
-    y_pred = np.argmax(logits, axis=1)
-    classes = labels.shape[1]
-    confusion = np.zeros((classes, classes))
-    for i in range(len(y_true)):
-        confusion[y_true[i], y_pred[i]] += 1
-    return confusion
+    return np.matmul(labels.T, logits)
