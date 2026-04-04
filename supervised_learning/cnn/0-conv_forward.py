dbd19af08cfbe0ba6ef570db5b4b0af36e23ed9b
Your Name <you@example.com>
2026-04-04 23:59:08 +0000

    s

---
+++ supervised_learning/cnn/0-conv_forward.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Convolution forward propagation function
+"""
+
+import numpy as np
+
+
+def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
+    """
+    Performs forward propagation over a convolutional layer of a neural network.
+
+    Args:
+        A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+                containing the output of the previous layer.
+        W: numpy.ndarray of shape (kh, kw, c_prev, c_new) containing
+           the kernels for the convolution.
+        b: numpy.ndarray of shape (1, 1, 1, c_new) containing the biases.
+        activation: activation function applied to the convolution.
+        padding: string either "same" or "valid" indicating the type of padding.
+        stride: tuple of (sh, sw) containing the strides for the convolution.
+
+    Returns:
+        The output of the convolutional layer.
+    """
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, _, c_new = W.shape
+    sh, sw = stride
+
+    if padding == "same":
+        ph = max((h_prev - 1) * sh + kh - h_prev, 0)
+        pw = max((w_prev - 1) * sw + kw - w_prev, 0)
+        pad_top = ph // 2
+        pad_bottom = ph - pad_top
+        pad_left = pw // 2
+        pad_right = pw - pad_left
+    elif padding == "valid":
+        pad_top = pad_bottom = pad_left = pad_right = 0
+    else:
+        raise ValueError("padding must be 'same' or 'valid'")
+
+    A_pad = np.pad(
+        A_prev,
+        ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right), (0, 0)),
+        mode="constant"
+    )
+
+    h_out = (h_prev + pad_top + pad_bottom - kh) // sh + 1
+    w_out = (w_prev + pad_left + pad_right - kw) // sw + 1
+    Z = np.zeros((m, h_out, w_out, c_new))
+
+    for i in range(m):
+        for h in range(h_out):
+            for w in range(w_out):
+                hs = h * sh
+                ws = w * sw
+                a_slice = A_pad[i, hs:hs + kh, ws:ws + kw, :]
+                for c in range(c_new):
+                    Z[i, h, w, c] = np.sum(a_slice * W[:, :, :, c]) + float(b[0, 0, 0, c])
+
+    return activation(Z)
