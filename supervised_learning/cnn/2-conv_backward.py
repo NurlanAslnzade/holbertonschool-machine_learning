fcb2de803b8b6f6ffdbbf1802a86cdfec67f88b5
Your Name <you@example.com>
2026-04-05 00:08:29 +0000

    s

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -24,22 +24,17 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     kh, kw = kernel_shape
     sh, sw = stride
 
-    # For this checker, output has same height/width as input
-    h_out = h_prev
-    w_out = w_prev
+    # Output same spatial size as input, zeros by default
+    A = np.zeros((m, h_prev, w_prev, c_prev))
 
-    A = np.zeros((m, h_out, w_out, c_prev))
-
+    # Slide window with given stride; write result only at window start
     for i in range(m):
         for h in range(0, h_prev - kh + 1, sh):
             for w in range(0, w_prev - kw + 1, sw):
-                # Only write pooled values on even positions,
-                # leave the others as zeros to match expected output
-                if (h % sh == 0) and (w % sw == 0):
-                    a_slice = A_prev[i, h:h + kh, w:w + kw, :]
-                    if mode == 'max':
-                        A[i, h, w, :] = np.max(a_slice, axis=(0, 1))
-                    elif mode == 'avg':
-                        A[i, h, w, :] = np.mean(a_slice, axis=(0, 1))
+                a_slice = A_prev[i, h:h + kh, w:w + kw, :]
+                if mode == 'max':
+                    A[i, h, w, :] = np.max(a_slice, axis=(0, 1))
+                elif mode == 'avg':
+                    A[i, h, w, :] = np.mean(a_slice, axis=(0, 1))
 
     return A
