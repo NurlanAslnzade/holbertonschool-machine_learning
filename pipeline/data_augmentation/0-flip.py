1baaa8ee805c745e7b3ee5e05532b86f201092b2
Your Name <you@example.com>
2026-04-09 20:33:39 +0000

    s

---
+++ pipeline/data_augmentation/0-flip.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module for image data augmentation: horizontal flip.
+
+This module provides a function to flip a 3D TensorFlow tensor horizontally.
+"""
+
+impor tensforflow as tf
+
+
+ def flip_image(image):
+     """sadasdasd asdasdad"""
+     return tf.image.flip_left_right(image)
+
